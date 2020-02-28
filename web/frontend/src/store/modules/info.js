d12405d9b3cd0fda5a8d4b0179f427a0c5c43202
Amir Mofasser <amir.mofasser@gmail.com>
2020-02-28 20:08:33 +0100

    Error handling for `k8sload` (#19)
    * Method `Load` in `pkg/kbz8s/load.go` returns any errors that the k8s client-go sdk might return
    * Implemented a simple Snackbar service using the Vuex store. To create a snack simply dispatch the object `{ message: string, color: string }` to the action `setSnack`. To clear snacks dispatch to action `clearSnack`.
    
    Co-authored-by: Robert Forsström <robert@middleware.se>

--- web/frontend/src/store/modules/info.js
+++ web/frontend/src/store/modules/info.js
@@ -1,4 +1,3 @@
-
 const state = {
     cGroup: {},
     httpheaders: {},
@@ -6,17 +5,17 @@ const state = {
     updated: null,
     statusCode: -1,
     k8sstats: {},
-    requestInfo: {}
+    requestInfo: {},
+    snack: {
+      message: '',
+      show: false,
+      color: 'success'
+    }
   }
  
-
-
-
 // getters
 const getters = {
-
   cgroup: (state) => {
-    
     return state.info.cGroup 
   },
   status: (state) => {
@@ -25,45 +24,52 @@ const getters = {
   updateTime: (state) => {
     return state.updated
   },
-
   headers: (state) => {
     return state.info.Headers
   },
-
   requestInfo: (state) => {
     return state.info.requestInfo
   }
 }
 
-
-  // actions
+// actions
 const actions = {
-
+  setSnack({ commit }, snack) {
+    commit('SET_SNACK', snack)
+  },
+  clearSnack({ commit }) {
+    commit('CLEAR_SNACK')
+  }
 }
 
 // mutations
 const mutations = {
- 
   'SET_INFO' (state, info) {
     state.updated = Date.now()
-
-      state.cGroup = info.cGroup
-      state.hostname = info.hostname
-      state.httpheaders = info.httpheaders
-      state.k8sstats = info.k8sstats
-      state.requestInfo = info.requestInfo
+    state.cGroup = info.cGroup
+    state.hostname = info.hostname
+    state.httpheaders = info.httpheaders
+    state.k8sstats = info.k8sstats
+    state.requestInfo = info.requestInfo
   },
   'SET_STATUS' (state, code) {
     state.statusCode = code
+  },
+  'SET_SNACK' (state, snack) {
+    state.snack.message = snack.message
+    state.snack.color = snack.color
+    state.snack.show = true
+  },
+  'CLEAR_SNACK' (state) {
+    state.snack.message = ''
+    state.snack.color = 'success'
+    state.snack.show = false
   }
-
-
-  
 }
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
-  }
+  state,
+  getters,
+  actions,
+  mutations
+}
